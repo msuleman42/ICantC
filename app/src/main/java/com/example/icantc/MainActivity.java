82a078db2b8a32b0438f72897f2eb565b00a3275
Munir Suleman <munirsuleman@hotmail.com>
2022-05-28 10:49:46 +0100

    Updating

--- app/src/main/java/com/example/icantc/MainActivity.java
+++ app/src/main/java/com/example/icantc/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    // Making sure this works
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
