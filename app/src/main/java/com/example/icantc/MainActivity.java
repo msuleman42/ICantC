8f2a77e37d7b57ad315f0c38a1c77766066b6d4c
1silvester <silvesterstephens@gmail.com>
2022-05-28 13:23:12 +0100

    This commit adds the functions of moving from the main page to the map page.

--- app/src/main/java/com/example/icantc/MainActivity.java
+++ app/src/main/java/com/example/icantc/MainActivity.java
@@ -2,16 +2,41 @@ package com.example.icantc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     // Making sure this works
     // Can confirm this works, bran stark
     // I am here, gossip girl
 
+    private Button button;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.toMapButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityMap();
+            }
+        });
+
     }
+
+    public void openActivityMap()
+    {
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
